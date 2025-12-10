2d79c1cec88f922b72bd18adab5d1a38b56cec9f
OzdoevaLeiila <tyuiu.ozdoevalm@outlook.com>
2025-12-11 00:32:08 +0500

    Task6

--- Tyuiu.OzdoevaLM.Sprint6.Task6.V4.Lib/DataService.cs
+++ Tyuiu.OzdoevaLM.Sprint6.Task6.V4.Lib/DataService.cs
@@ -9,7 +9,6 @@ namespace Tyuiu.OzdoevaLM.Sprint6.Task6.V4.Lib
         {
             string[] a = File.ReadAllLines(path);
             string str = "";
-            string n = "n";
             string res = "";
 
             foreach (string line in a)
@@ -17,11 +16,11 @@ namespace Tyuiu.OzdoevaLM.Sprint6.Task6.V4.Lib
                 str = str + " " + line;
             }
 
-            string[] words = str.Split(' ').ToArray();
+            string[] words = str.Split(new char[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries);
 
             for (int i = 0; i < words.Length; i++)
             {
-                if (words[i].ToLower().Contains(n))
+                if (words[i].ToLower().Contains("n"))
                 {
                     res = res + " " + words[i];
                 }
