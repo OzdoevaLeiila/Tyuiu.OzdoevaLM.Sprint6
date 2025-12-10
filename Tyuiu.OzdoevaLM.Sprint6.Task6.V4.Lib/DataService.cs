79d7efc55852a606c575af0e713d6c7aaaabfa78
OzdoevaLeiila <tyuiu.ozdoevalm@outlook.com>
2025-12-11 00:26:54 +0500

    Task6

--- Tyuiu.OzdoevaLM.Sprint6.Task6.V4.Lib/DataService.cs
+++ Tyuiu.OzdoevaLM.Sprint6.Task6.V4.Lib/DataService.cs
@@ -9,6 +9,7 @@ namespace Tyuiu.OzdoevaLM.Sprint6.Task6.V4.Lib
         {
             string[] a = File.ReadAllLines(path);
             string str = "";
+            string n = "n";
             string res = "";
 
             foreach (string line in a)
@@ -20,8 +21,7 @@ namespace Tyuiu.OzdoevaLM.Sprint6.Task6.V4.Lib
 
             for (int i = 0; i < words.Length; i++)
             {
-                // Ищем 'n' или 'N' - приводим к нижнему регистру
-                if (words[i].ToLower().Contains("n"))
+                if (words[i].ToLower().Contains(n))
                 {
                     res = res + " " + words[i];
                 }
