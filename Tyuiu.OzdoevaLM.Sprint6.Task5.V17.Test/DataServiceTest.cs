7c5e02cff014e49fbc4f4b823dffd9aab511bae8
OzdoevaLeiila <tyuiu.ozdoevalm@outlook.com>
2025-12-10 23:16:43 +0500

    Task5

---
+++ Tyuiu.OzdoevaLM.Sprint6.Task5.V17.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Tyuiu.OzdoevaLM.Sprint6.Task5.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.OzdoevaLM.Sprint6.Task5.V17/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.OzdoevaLM.Sprint6.Task5.V17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
